85d5208cd2596763d874420d74f315d34f8c09f6
Tim Arnold <tim@weareoutright.com>
2026-03-07 22:59:54 -0500

    Switch Sanity data client from next-sanity to @sanity/client to bypass Next.js fetch cache

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -1,4 +1,4 @@
-import { createClient } from 'next-sanity'
+import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 import { SanityImageSource } from '@sanity/image-url/lib/types/types'
 
